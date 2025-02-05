b2e4cdffb3161525d861110343a11d2912b5252a
emiranivala <taitufam@gmail.com>
2025-02-05 22:19:31 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -374,7 +374,10 @@ async def get_msg(userbot, sender, edit_id, msg_link, i, message):
             await app.edit_message_text(sender, edit_id, "Have you joined the channel?")
             return
         except Exception as e:
-            await app.edit_message_text(sender, edit_id, f". Error: {e}")
+            if "PEER_ID_INVALID" in str(e):
+                await app.edit_message_text(sender, edit_id, ".")
+            else:
+                await app.edit_message_text(sender, edit_id, f". Error: {e}")
     
     else:
         edit = await app.edit_message_text(sender, edit_id, "Cloning...")
@@ -685,3 +688,4 @@ async def handle_user_input(event):
             await event.respond(f"Words added to delete list: {', '.join(words_to_delete)}")
 
         del sessions[user_id]
+ 
